13db85ca0e9a371cfcbf14f0c3f7eec7f82b067f
shabelnikilya <shabelnik_ilya@mail.ru>
2022-02-24 11:03:52 +0300

    0. Spring Liquibase [#302330]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,20 +1,20 @@
-CREATE TABLE authorities (
+CREATE TABLE IF NOT EXISTS authorities (
   id serial primary key,
-  authority VARCHAR(50) NOT NULL unique
+  authority VARCHAR(50) not null unique
 );
 
 create table if not exists users (
     id serial primary key,
-    username text not null unique,
-    password text not null,
+    username VARCHAR(100) not null unique,
+    password VARCHAR(150) not null,
     enabled boolean default true,
     authority_id int not null references authorities(id)
 );
 
 create table if not exists posts (
   id serial primary key,
-  name varchar(2000),
-  description text,
+  name varchar(200),
+  description VARCHAR(500),
   created timestamp without time zone not null default now(),
   users_id int not null references users(id)
 );
